d00be428a788a0cae0dfa081b9efa0d74f844fb9
maureliosiqueira <marcoa.siqueira@gmail.com>
2025-10-05 17:10:13 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,21 @@
-// 🔑 Substitua pelas suas chaves do Firebase!
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD...",
+  apiKey: "AIzaSyCZ5Pgzv08pcR6Xfp-SdjdYmqqSw7brNQ4",
   authDomain: "estudo-ativo-projeto.firebaseapp.com",
   projectId: "estudo-ativo-projeto",
-  storageBucket: "estudo-ativo-projeto.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef1234567890"
+  storageBucket: "estudo-ativo-projeto.firebasestorage.app",
+  messagingSenderId: "257330913879",
+  appId: "1:257330913879:web:4babb1b1fa4a77cd7bb912",
+  measurementId: "G-SJT94J9CP7"
 };
 
-// Inicializa o Firebase
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
