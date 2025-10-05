4bf02ca7e586d39133f08bea696bd893e7254ca7
maureliosiqueira <marcoa.siqueira@gmail.com>
2025-10-05 14:13:01 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,12 @@
-const firebaseConfig = { apiKey: 'YOUR_API_KEY', authDomain: 'your-project.firebaseapp.com' };
+// 🔑 Substitua pelas suas chaves do Firebase!
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef1234567890"
+};
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
