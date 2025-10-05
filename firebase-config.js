9acee102e1e18052239953bd25a39fbb7e85a8ab
maureliosiqueira <marcoa.siqueira@gmail.com>
2025-10-05 15:27:01 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // 🔑 Substitua pelas suas chaves do Firebase!
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
+  apiKey: "AIzaSyD...",
+  authDomain: "estudo-ativo-projeto.firebaseapp.com",
+  projectId: "estudo-ativo-projeto",
+  storageBucket: "estudo-ativo-projeto.appspot.com",
   messagingSenderId: "1234567890",
   appId: "1:1234567890:web:abcdef1234567890"
 };
